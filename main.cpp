00b1db8d226fef923cf4f71195eb0878ac4a1876
Ivars Rozhleys <noname@noname.com>
2019-03-07 20:00:40 +0300

    Добавлены прототипы методов для токенизации и сохранения токенов в файл.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <set>
+
+bool tokenize(std::ifstream& fin, std::set<std::string>& tokens)
+{
+	fin.seekg(0, std::ios::beg);
+	while(true)
+	{
+		std::string token;
+		fin >> token;
+		if(fin.eof())
+			break;
+		tokens.insert(token);
+	}
+	return !tokens.empty();
+}
+
+bool save(std::ofstream& fout, std::set<std::string> const& tokens)
+{
+	fout.seekp(0, std::ios::end);
+	std::set<std::string>::const_iterator iter = tokens.cbegin();
+	std::set<std::string>::const_iterator const end = tokens.cend();
+	while(iter != end)
+	{
+		fout << *iter << std::endl;
+		++iter;
+	}
+	return true;
+}
 
 int main(int argc, char** argv)
-{	
+{
 	setlocale(LC_ALL, "Russian");
 
 	if(argc != 3)
@@ -12,33 +40,37 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
-	std::string const fnIn(argv[1]);
-	std::string const fnOut(argv[2]);
+	std::set<std::string> tokens;
 
-	std::ifstream fin;
-	std::ofstream fout;
-
-	fin.open(fnIn, std::ios::in);
-	fout.open(fnOut, std::ios::out);
-
-	if(!fin)
 	{
-		std::cout << "Не могу открыть файл для чтения: " << fnIn << std::endl;
-		goto _runaway;
-	}
-
-	if(!fout)
-	{
-		std::cout << "Не могу открыть файл для записи: " << fnOut << std::endl;
-		goto _runaway;
+		std::ifstream fin;
+		fin.open(argv[1], std::ios::in);
+		if(fin)
+		{
+			std::cout << "Обрабатываю файл: " << argv[1] << std::endl;
+			tokenize(fin, tokens);
+			fin.close();
+		}
+		else
+		{
+			std::cout << "Не могу открыть файл для чтения: " << argv[1] << std::endl;
+		}
 	}
 
 	{
-		std::cout << "Обрабатываю файл: " << fnIn << std::endl;
+		std::ofstream fout;
+		fout.open(argv[2], std::ios::out | std::ios::app);
+
+		if(fout)
+		{
+			save(fout, tokens);
+			fout.close();
+		}
+		else
+		{
+			std::cout << "Не могу открыть файл для записи: " << argv[2] << std::endl;
+		}
 	}
 
-	_runaway:
-	fin.close();
-	fout.close();
 	return 0;
 }
