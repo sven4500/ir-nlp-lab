a4d6e80cba535ed171c774bfef9a947f44faddb5
Ivars Rozhleys <sven4500@mail.ru>
2019-05-04 21:04:05 +0300

    Составил соответствующие таблицы метрик и завершил написание отчёта.

--- compmet.m
+++ compmet.m
@@ -1,15 +1,14 @@
-% r - оценки докупентов в ранжированном списке
-% k - уровень метрики
+% r - вектор оценок ранжированного списка
+% k - уровень метрик
 % gmax - максимально возможная оценка
 function y = compmet(r,k,gmax)
 
-    % Реализация функций метрик.
     p=@(r,k,gmax)sum(r(1:k)>gmax./2)./length(r);
     
     cg=@(r,k)sum(r(1:k));
     dcg=@(r,k)sum(r(1:k)./log2((1:k)+1));
     idcg=@(r,k)sum(sort(r(1:k),'descend')./log2((1:k)+1));
-    ndcg=@(r,k)dcg(r,k)/idcg(r,k);
+    ndcg=@(r,k)dcg(r,k)./idcg(r,k);
     
     R=@(g,gmax)(2.^g-1)./(2.^gmax);
     err=@(r,k,gmax)sum((1./(1:k)).*R(r(1:k),gmax).*prod(tril(repmat(1-R(r(1:k),gmax),k,1),-1)+triu(ones(k)),2)');
