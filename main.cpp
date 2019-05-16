86f75f47d3b345a2872f798898683c8b995fba15
Ivars Rozhleys <sven4500@mail.ru>
2019-05-16 20:42:31 +0300

    Проектный файл и файл main.

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc, char** argv)
+{
+	//std::locale(LC_CTYPE, "Russian");
+
+	if(argc != 2)
+	{
+		std::cout << "NLP2.exe токены.xml" << std::endl;
+		return -1;
+	}
+
+	//std::ofstream fout();
+	//fout.close();
+	return 0;
+}
