8a04778b40dad086872323f399a0d2ac6079b419
Ivars Rozhleys <sven4500@mail.ru>
2019-05-19 21:21:22 +0300

    В скрипт MATLAB добавлен счёт коэффициентов закона Мандельброта.

--- ZipfGraph.m
+++ ZipfGraph.m
@@ -7,7 +7,17 @@ end
 A = fscanf(fd, '%d');
 fclose(fd);
 X = 1:length(A);
-%A=A./max(A);
+% B - некоторый идеальный закон.
 B = max(A) ./ X;
-plot(X,A,X,B);
+% logb - функция логарифмирования по произвольному основанию.
+logb=@(x,b)(log(x)./log(b));
+% C - закон Мандельброта вида M(r)=p*r^(-b).
+% m - количество первых значимых рангов.
+% p - максимальная частота слова.
+Cm = 100;
+Cp = A(1);
+Cb = -1 .* logb(A(Cm) ./ Cp, Cm);
+sprintf('p=%f, b=%f', Cp, Cb)
+C = Cp .* X .^ (-Cb);
+plot(X,A,X,B,X,C);
 pbaspect([1 1 1]);
