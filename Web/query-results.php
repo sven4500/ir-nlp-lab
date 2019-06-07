c18171f4c427fcbb82b6bca6c51ad09029248850
Ivars Rozhleys <sven4500@mail.ru>
2019-06-07 23:48:33 +0300

    Добавлено формирование списка ссылок на документы и скрипт PHP для извлечения содержимого из XML и его отображения.

--- Web/query-results.php
+++ Web/query-results.php
@@ -1,7 +1,11 @@
 ﻿<?php
 	$query = $_GET['query'];
-	print("<b>Запрос:</b> $query<br>");
 	execute('IR5.exe "Corpus/index.dat" "Corpus/posindex.dat"', $query);
+	
+	function makeLink($pageId) {
+		echo '<a href="open-page.php?pageId=', $pageId, '">', $pageId ,'</a><br>';
+	}
+	
 	function execute($exe, $query) {
 		$exe_cmd = escapeshellcmd($exe);
 		$pipe_desc = array(array('pipe', 'r'), array('pipe', 'w'), array('pipe', 'w'));
@@ -14,11 +18,15 @@
 		$msTime = fgets($pipes[1]);
 		fclose($pipes[1]);
 		fclose($pipes[2]);
-
-		print("<b>Документы:</b> $result<br>");
-		print("<b>Время поиска (мс):</b> $msTime<br>");
-
+		
 		$ret_val = proc_close($process);
-		print("<b>Ошибка:</b> $ret_val<br>");
+		
+		echo "<b>Запрос:</b> $query<br>";
+		echo "<b>Время поиска (мс):</b> $msTime<br>";
+		
+		$result = explode(" ", $result);
+		$c = min(count($result), 50);
+		for($i = 0; $i < $c; ++$i)
+			makeLink($result[$i]);
 	}
 ?>
