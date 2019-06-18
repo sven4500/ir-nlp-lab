054032207e2d81eace4ff044e1c8448a31a01e19
Ivars Rozhleys <sven4500@mail.ru>
2019-06-18 23:41:39 +0300

    Полностью отказался от хранения строк. Посмотрим что из этого получиться...

--- normIndexMaker.cpp
+++ normIndexMaker.cpp
@@ -6,20 +6,6 @@
 #include "crc32.h"
 using namespace std;
 
-size_t NormIndexMaker::equalSize(string const& s1, string const& s2)
-{
-    size_t const size = (min(s1.size(), s2.size()) / 2) * 2;
-    if(size < 2)
-        return 0;
-
-    size_t i = 0;
-    for(i = 0; i < size; i += 2)
-        if(*(short*)&s1[i] != *(short*)&s2[i])
-            break;
-
-    return i;
-}
-
 NormIndexMaker::NormIndexMaker()
 {
 
@@ -36,7 +22,8 @@ void NormIndexMaker::addTokenAsTerm(string const& token, unsigned int const docI
     for(size_t i = 0, size = token.size(); i <= threshold && i < size; i += 2)
     {
         string const term = token.substr(0, size-i);
-        vector<unsigned int>& vect = _termToDocID[term];
+        unsigned int const hash = crc32(0, &term[0], term.size());
+        vector<unsigned int>& vect = _termToDocID[hash];
         if(std::find(vect.begin(), vect.end(), docID) == vect.end())
             vect.push_back(docID);
     }
@@ -67,30 +54,4 @@ bool NormIndexMaker::update(tinyxml2::XMLElement const* const elem)
 	}
 
 	return !_termToDocID.empty();
-}
-
-bool NormIndexMaker::write(std::string const& filename)
-{
-    std::cout << std::endl << "Всего терминов проиндексировано "
-        << _termToDocID.size() << std::endl;
-
-    #if defined(_DEBUG)
-    {
-        std::ofstream fout;
-        fout.open("normtokens.txt", std::ios::trunc | std::ios::out);
-        for(auto iter = _termToDocID.begin(), end = _termToDocID.end(); iter != end; ++iter)
-            fout << iter->first << std::endl;
-        fout.close();
-    }
-    #endif
-
-    std::cout << "Подготавливаю данные для записи..." << std::endl;
-    for(map<string, vector<unsigned int>>::iterator iter = _termToDocID.begin(), end = _termToDocID.end(); iter != end; ++iter)
-    {
-        unsigned int const termID = crc32(0, iter->first.c_str(), iter->first.size());
-        InvertedIndexMaker::_termToDocID[termID] = iter->second;
-    }
-
-    // Вызываем родительскую реализацию метода write.
-    return InvertedIndexMaker::write(filename);
 }
--- normIndexMaker.h
+++ normIndexMaker.h
@@ -13,17 +13,10 @@ public:
     NormIndexMaker();
     ~NormIndexMaker();
 
-    // Сравнивает две строки и находит количество совпадающих байт. Количество
-    // байт возвращает всегда кратное двум.
-    static std::size_t equalSize(std::string const& s1, std::string const& s2);
-
     virtual bool update(tinyxml2::XMLElement const* elem);
-    virtual bool write(std::string const& filename);
 
 protected:
     void addTokenAsTerm(std::string const& token, unsigned int docID);
-
-    std::map<std::string, std::vector<unsigned int>> _termToDocID;
 
 };
 
